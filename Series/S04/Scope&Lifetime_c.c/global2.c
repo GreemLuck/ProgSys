9e11157c633e13c6b8f2cd2985d79283513a7bb5
Gremaud Lucien <lucien.gremaud@gmail.com>
2019-03-15 15:00:47 +0100

    start of serie 4

---
+++ Series/S04/Scope&Lifetime_c.c/global2.c
@@ -0,0 +1,25 @@
+/*----------------------------------------------------------------------------
+DESCRIPTION : see global1.c
+   
+Beat Hirsbrunner, 25 October 2011, v 1.0; rev. 12 October 2012
+----------------------------------------------------------------------------*/
+#include <stdio.h>
+
+int g_v = 2;            // global (declared as extern in file global1.c and defined here)
+char c;
+
+static int sg_v = 2;    // semi-global (is defined here)
+static int sg_f(int x); // semi-global (must be defined in this file)
+
+int g_f() // global (declared in file global1.c and defined here)
+{
+   printf(" g_f: g_v = %i, sg_v = %i\n", g_v, sg_v);
+   sg_f(sg_v);
+   return 0;
+}
+
+static int sg_f(int x) // semi-global
+{
+   printf("sg_f defined in file global2.c: Hello %i\n", x);
+   return 0;
+}
